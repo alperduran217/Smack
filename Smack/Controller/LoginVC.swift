5530fe9a70d8e9763ee7239fa0e83f9b49bdedbc
alperduran217 <alperduran217@gmail.com>
2017-10-28 01:12:57 +0200

    Login Page added

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mehmet Alper Duran on 28.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+ 
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Mehmet Alper Duran on 28.10.2017.
+//  Copyright © 2017 Mehmet Alper Duran. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
