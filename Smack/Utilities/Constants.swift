91ef8e082cf58c9cddcdbed5559c6a5ab86dfdb0
alperduran217 <alperduran217@gmail.com>
2017-11-03 17:44:26 +0100

    Created a web request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> () // renaming a type. Creating custom closures. Closures: 
+
+// URL Constants
+
+let BASE_URL = "https://git.heroku.com/chat-tutorial-ios-.git/v1"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User DefaultsFoundation
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_MAIL = "userEmail"
